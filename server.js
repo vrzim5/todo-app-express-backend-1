5963f050ae67c483184c9a3c5fc52099c8cfbd13
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-08 21:51:35 -0300

    feat: :sparkles: Configura conexão com banco de dados utilizando o Mongoose
    Este commit configura o projeto para conexão com o banco de dados MongoDB utilizando o pacote Mongoose.

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log(`Connected to MongoDB...`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
