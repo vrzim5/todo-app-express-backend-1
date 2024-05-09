68884e658bc72cd45879868ac20528aeb78fc5e6
Renan Costa Alencar <renan.costaalencar@gmail.com>
2024-05-08 21:22:37 -0300

    feat: :tada: Cria o projeto inicial de backend com NodeJS, Express e Mongoose
    Este commit cria o projeto inicial para o desenvolvimento do backend da aplicação de tarefas utilizando NodeJS, Express e Mongoose.

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
